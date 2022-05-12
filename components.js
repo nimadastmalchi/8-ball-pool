2e7d826da7867e41d22876a4947bcf4e7f0d043a
alexwazzan <alexwazzan1@gmail.com>
2022-05-12 13:17:08 -0700

    Another attempt at fixing collision detection

--- components.js
+++ components.js
@@ -9,12 +9,11 @@ const BALL_SHAPE = new defs.Subdivision_Sphere(4);
 const BALL_MATERIAL = new Material(new defs.Phong_Shader(), { ambient: 1, diffusivity: .6, color: hex_color("#FFFFFF") });
 
 const COLLISION_VEL_LOSS = 0.9;
-const FRICTION_VEL_LOSS = 1;
+const FRICTION_VEL_LOSS = 0.99;
 
 export class Ball {
     constructor(init_loc, init_vel, color, solid) {
-        this.model_transform = Mat4.identity();
-        this.model_transform = this.model_transform.times(Mat4.translation(init_loc[0], init_loc[1], init_loc[2])).times(Mat4.scale(BALL_RADIUS, BALL_RADIUS, BALL_RADIUS));
+        this.model_transform = Mat4.translation(init_loc[0], init_loc[1], init_loc[2]).times(Mat4.scale(BALL_RADIUS, BALL_RADIUS, BALL_RADIUS));
         this.loc = init_loc;
         this.vel = init_vel;
         this.solid = solid;
@@ -23,6 +22,10 @@ export class Ball {
 
     get_loc() {
         return this.loc;
+    }
+
+    set_loc(new_loc) {
+        this.loc = new_loc;
     }
 
     get_vel() {
@@ -36,7 +39,8 @@ export class Ball {
     update_loc(dt) {
         let dl = this.vel.times(dt);
         this.loc = this.loc.plus(dl);
-        this.model_transform = this.model_transform.times(Mat4.translation(dl[0], dl[1], dl[2]));
+        this.model_transform = Mat4.translation(this.loc[0], this.loc[1], this.loc[2]);
+        this.set_vel(this.get_vel().times(FRICTION_VEL_LOSS));
 
         // Ensure ball is within bounds:
     }
@@ -49,55 +53,40 @@ export class Ball {
 export class Game {
     constructor() {
         this.balls = Array(4);
-        this.balls[0] = new Ball(vec3(4, 4, 0), vec3(-1, -1, 0), hex_color("#FFFFFF"), false);
-        this.balls[1] = new Ball(vec3(-4, 4, 0), vec3(1, -1, 0), hex_color("#FFFFFF"), false);
-        this.balls[2] = new Ball(vec3(4, -4, 0), vec3(-1, 1, 0), hex_color("#FFFFFF"), false);
-        this.balls[3] = new Ball(vec3(-4, -4, 0), vec3(1, 1, 0), hex_color("#FFFFFF"), false);
-        this.balls[4] = new Ball(vec3(0, 4, 0), vec3(-1, -1, 0), hex_color("#FFFFFF"), false);
+        this.balls[0] = new Ball(vec3(0, 0, 0), vec3(0, 0, 0), hex_color("#FFFFFF"), false);
+        this.balls[1] = new Ball(vec3(1.1 * BALL_RADIUS, 2 * BALL_RADIUS, 0), vec3(0, 0, 0), hex_color("#FFFFFF"), false);
+        this.balls[2] = new Ball(vec3(-1.1 * BALL_RADIUS, 2 * BALL_RADIUS, 0), vec3(0, 0, 0), hex_color("#FFFFFF"), false);
+        this.balls[3] = new Ball(vec3(0, -10, 0), vec3(0, 10, 0), hex_color("#FFFFFF"), false);
     }
 
     update(dt) {
-        this.collision_check();
+        this.collision_check(dt);
         for (let i = 0; i < this.balls.length; ++i) {
             this.balls[i].update_loc(dt);
         }
     }
 
-    collision_check() {
-        let new_vels = Array(this.balls.length);
-
-        for (let i = 0; i < this.balls.length; ++i) {
-            new_vels[i] = vec3(0, 0, 0);
-        }
-
+    collision_check(dt) {
         for (let i = 0; i < this.balls.length; ++i) {
             for (let j = i + 1; j < this.balls.length; ++j) {
                 let loc1 = this.balls[i].get_loc();
                 let loc2 = this.balls[j].get_loc();
                 let dist_vec = loc1.minus(loc2);
-                let dist_squared = dist_vec.dot(dist_vec);
-                if (dist_squared <= (2 * BALL_RADIUS) ** 2) {
+                let dist = dist_vec.norm();
+                if (dist <= 2 * BALL_RADIUS) {
+                    let revert = dist_vec.times((2 * BALL_RADIUS - dist) / dist);
+                    this.balls[i].set_loc(loc1.plus(revert.times(1 / 2)));
+                    this.balls[j].set_loc(loc2.minus(revert.times(1 / 2)));
+
                     let old_vel1 = this.balls[i].get_vel();
                     let old_vel2 = this.balls[j].get_vel();
 
-                    let new_vel1 = old_vel1.minus(loc1.minus(loc2).times(old_vel1.minus(old_vel2).dot(loc1.minus(loc2)) / dist_squared));
-
-                    let new_vel2 = old_vel2.minus(loc2.minus(loc1).times(old_vel2.minus(old_vel1).dot(loc2.minus(loc1)) / dist_squared));
-
-                    new_vels[i] = new_vels[i].plus(new_vel1);
-                    new_vels[j] = new_vels[j].plus(new_vel2);
+                    this.balls[i].set_vel(old_vel1.minus(loc1.minus(loc2).times(old_vel1.minus(old_vel2).dot(loc1.minus(loc2)) / dist ** 2)).times(COLLISION_VEL_LOSS));
+                    this.balls[j].set_vel(old_vel2.minus(loc2.minus(loc1).times(old_vel2.minus(old_vel1).dot(loc2.minus(loc1)) / dist ** 2)).times(COLLISION_VEL_LOSS)); 
                 }
             }
         }
-
-        for (let i = 0; i < this.balls.length; ++i) {
-            if (new_vels[i][0] != 0 || new_vels[i][1] != 0 || new_vels[i][2] != 0) {
-                this.balls[i].set_vel(new_vels[i].times(COLLISION_VEL_LOSS));
-                //this.balls[i].update_loc(0.02);
-            }
-        }
     }
-
 
     draw(context, program_state) {
         for (let i = 0; i < this.balls.length; ++i) {
