82b8aad8ab6d8209d9b32806bce92d9b2aa8f35c
Nima Amir Dastmalchi <nima.dastmalchi@gmail.com>
2022-05-28 23:38:25 -0700

    Removed ugly lines

--- shaders.js
+++ shaders.js
@@ -44,13 +44,9 @@ export class Ball_Phong extends defs.Textured_Phong {
             uniform vec2 vel;
             void main(){
                 vec2 n_f_tex_coord = vec2(f_tex_coord.x + vel[0], f_tex_coord.y + vel[1]);
-
                 vec4 tex_color = texture2D( texture, n_f_tex_coord );
-
                 if( tex_color.w < .01 ) discard;
-                                                                         // Compute an initial (ambient) color:
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                         // Compute the final color with contributions from lights:
                 gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
         } `;
     }
