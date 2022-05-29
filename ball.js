f6450cee26234870d07b890c0607d137fae978d6
Nima Amir Dastmalchi <nima.dastmalchi@gmail.com>
2022-05-29 00:06:05 -0700

    Fixed rolling

--- ball.js
+++ ball.js
@@ -13,6 +13,7 @@ export class Ball {
         this.vel = init_vel;
         this.solid = solid;
         this.texture = texture;
+        this.texture_displacement = [ Math.random(), Math.random() ]
         this.pocket = null;
         this.visible = true;
     }
@@ -113,9 +114,9 @@ export class Ball {
     }
 
     draw(context, program_state) {
-        // Function set_loc() may be called after the update() function, so ensure this.model_transform is up to date:
         this.model_transform = Mat4.translation(this.loc[0], this.loc[1], this.loc[2]).times(Mat4.rotation(-Math.PI / 2, 0, 1, 0));
-        BALL_SHADER.set_vel([this.vel[0], this.vel[1]]);
+        this.texture_displacement = [this.texture_displacement[0] - this.vel[0]/700, this.texture_displacement[1] - this.vel[1]/700]
+        BALL_SHADER.set_texture_displacement(this.texture_displacement);
         BALL_SHAPE.draw(context, program_state, this.model_transform, BALL_MATERIAL.override({ texture: this.texture }));
     }
 }
--- shaders.js
+++ shaders.js
@@ -24,16 +24,16 @@ export class Table_Phong extends defs.Phong_Shader {
 export class Ball_Phong extends defs.Textured_Phong {
     constructor(num_lights = 2) {
         super(num_lights);
-        this.vel = [ 0.0, 0.0 ];
+        this.texture_displacement = [ 0.0, 0.0 ];
     }
 
-    set_vel(vel) {
-        this.vel = vel;
+    set_texture_displacement(texture_displacement) {
+        this.texture_displacement = texture_displacement;
     }
 
     send_material(gl, gpu, material) {
         super.send_material(gl, gpu, material);
-        gl.uniform2fv(gpu.vel, this.vel);
+        gl.uniform2fv(gpu.texture_displacement, this.texture_displacement);
     }
 
     fragment_glsl_code() {
@@ -41,10 +41,10 @@ export class Ball_Phong extends defs.Textured_Phong {
             varying vec2 f_tex_coord;
             uniform sampler2D texture;
             uniform float speed;
-            uniform vec2 vel;
+            uniform vec2 texture_displacement;
             void main(){
-                vec2 n_f_tex_coord = vec2(f_tex_coord.x + vel[0], f_tex_coord.y + vel[1]);
-                vec4 tex_color = texture2D( texture, n_f_tex_coord );
+                vec2 new_f_tex_coord = vec2( f_tex_coord.x + texture_displacement[0], f_tex_coord.y + texture_displacement[1] );
+                vec4 tex_color = texture2D( texture, new_f_tex_coord );
                 if( tex_color.w < .01 ) discard;
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
                 gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
